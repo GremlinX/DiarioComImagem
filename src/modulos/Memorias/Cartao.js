0d74106d2ba95836cf7e709f347ac634039a4cfa
Gremlin <50713742+GremlinX@users.noreply.github.com>
2023-06-11 00:16:26 -0300

    Corrige envio de imagem null

--- src/modulos/Memorias/Cartao.js
+++ src/modulos/Memorias/Cartao.js
@@ -3,11 +3,13 @@ import style from './memoria.module.css'
 
 function Cartao({ creator, title, comment, image }) {
     
+    const defaultImageUrl = 'https://mrconfeccoes.com.br/wp-content/uploads/2018/03/default.jpg';
+
     return (
         <div className="p-1"> 
             <app-cartao>
                 <Card style={{ width: '18rem', color: 'black'}}>
-                    <Card.Img className={style.cardImg} variant="top" src={URL.createObjectURL(image)} alt={title}/>
+                    <Card.Img className={style.cardImg} variant="top" src={image || defaultImageUrl} alt={title} />
                     <Card.Body>
                         <Card.Title>{title}</Card.Title>
                         <Card.Text>{comment}</Card.Text>
